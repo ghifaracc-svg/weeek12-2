b919d105f93ee69025b244f6158dd13f59078589
Muhammad Abu Dzar Al Ghifari <ghifaracc@gmail.com>
2026-05-08 17:22:57 +0700

    Rename Node class to Node17 and update constructor

--- Node.java
+++ Node.java
@@ -1,8 +1,8 @@
-public class Node {
+public class Node17 {
     Student data;
     Node next;
 
-    Node(Student data, Node next) {
+    Node17(Student data, Node next) {
         this.data = data;
         this.next = next;
     }
